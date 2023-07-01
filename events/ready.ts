885bb9d96b4175c1aef35ac6d940b586df678315
opticXC <aabishmalik018@gmail.com>
2023-07-01 23:23:56 +0530

    init

--- events/ready.ts
+++ events/ready.ts
@@ -1,8 +1,9 @@
+// deno-lint-ignore-file no-explicit-any
 import {
   Bot,
   DiscordReady,
 } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 
-export function onReady(b: Bot, payload, readyEvent: DiscordReady) {
+export function onReady(_b: Bot, _payload: any, readyEvent: DiscordReady) {
   console.log(`Logged in as ${readyEvent.user.username} `);
 }
